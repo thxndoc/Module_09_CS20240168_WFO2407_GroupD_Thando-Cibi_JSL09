910ec3ccebebc8e1ba45516a4920463e6c33181d
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-25 20:39:14 +0200

    Update with await

--- index.js
+++ index.js
@@ -8,57 +8,54 @@ const weather = document.getElementById("weather");
 // URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
 // (Change the "query" at the end to preferred theme)
 // Change the body's backgroundImage to: `url(<insert the URL of the image from the API here>)`
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=textures-patterns")
-    .then(response => response.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`;
-        //display author's name
-        author.textContent = `Photo by: ${data.user.name}`;
-    })
-    .catch(err => {
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1613679169453-ef00f3730ed4?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjcyNzM2NjF8&ixlib=rb-4.0.3&q=85)`;
-        author.textContent = `Photo by: Wolfgang Hasselmann`;
-    })
+const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=textures-patterns")
+const data = await response.json()
+document.body.style.backgroundImage = `url(${data.urls.full})`;
+//display author's name
+author.textContent = `Photo by: ${data.user.name}`;
+    
+    // .catch(err => {
+    //     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1613679169453-ef00f3730ed4?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjcyNzM2NjF8&ixlib=rb-4.0.3&q=85)`;
+    //     author.textContent = `Photo by: Wolfgang Hasselmann`;
+    // })
 
 // Pull down cryptocurrency data for dogecoin from the CoinGecko API
 // https://api.coingecko.com/api/v3/coins/dogecoin
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(response => {
-        if (!response.ok) {
-            throw Error("Something went wrong")
-        }
-        return response.json()
-    })
-    .then(data => {
-        // Create image element and append to cryptoTop div
-        const imgEl = document.createElement("img");
-        imgEl.src = data.image.small;
-        cryptoTop.appendChild(imgEl);
+const response = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+if (!response.ok) {
+    throw Error("Something went wrong")
+}
+const data = await response.json()
+    
+// Create image element and append to cryptoTop div
+const imgEl = document.createElement("img");
+imgEl.src = data.image.small;
+cryptoTop.appendChild(imgEl);
 
-        // Create span element and append to cryptoTop div
-        const spanEl = document.createElement("span");
-        spanEl.textContent = data.name;
-        cryptoTop.appendChild(spanEl);
+// Create span element and append to cryptoTop div
+const spanEl = document.createElement("span");
+spanEl.textContent = data.name;
+cryptoTop.appendChild(spanEl);
 
-        
-        // Create current price paragraph element and append to crypto div
-        const currentPriceEl = document.createElement("p");
-        currentPriceEl.textContent = `🎯: $${data.market_data.current_price.usd}`;
-        crypto.appendChild(currentPriceEl);
-        
 
-        // Create high price paragraph element and append to crypto div
-        const highPriceEl = document.createElement("p");
-        highPriceEl.textContent = `👆: $${data.market_data.high_24h.usd}`;
-        crypto.appendChild(highPriceEl);
+// Create current price paragraph element and append to crypto div
+const currentPriceEl = document.createElement("p");
+currentPriceEl.textContent = `🎯: $${data.market_data.current_price.usd}`;
+crypto.appendChild(currentPriceEl);
 
-        // Create low price paragraph element and append to crypto div
-        const lowPriceEl = document.createElement("p");
-        lowPriceEl.textContent = `👇: $${data.market_data.low_24h.usd}`;
-        crypto.appendChild(lowPriceEl);
 
-    })
-    .catch(err => console.error(err))
+// Create high price paragraph element and append to crypto div
+const highPriceEl = document.createElement("p");
+highPriceEl.textContent = `👆: $${data.market_data.high_24h.usd}`;
+crypto.appendChild(highPriceEl);
+
+// Create low price paragraph element and append to crypto div
+const lowPriceEl = document.createElement("p");
+lowPriceEl.textContent = `👇: $${data.market_data.low_24h.usd}`;
+crypto.appendChild(lowPriceEl);
+
+    
+   // .catch(err => console.error(err))
 
 // Display current time as the following format: 17:30
 function getCurrentTime() {
@@ -70,16 +67,12 @@ function getCurrentTime() {
 setInterval(getCurrentTime, 1000);
 
 // Get current position
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-        .then(response => {
-            if (!response.ok) {
-                throw Error("Weather data not available")
-            }
-            return response.json()
-        })
-        .then(data => {
-            console.log(data)
+navigator.geolocation.getCurrentPosition(async position => {
+const response = fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+if (!response.ok) {
+    throw Error("Weather data not available")
+}
+const data = await response.json()
             // Display weather icon
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             const icon = document.createElement("img");
@@ -98,6 +91,5 @@ navigator.geolocation.getCurrentPosition(position => {
             locationEl.classList.add("city");
             weather.appendChild(locationEl);
 
-        })
-        .catch(err => console.error(err))
+        //.catch(err => console.error(err))
 });
