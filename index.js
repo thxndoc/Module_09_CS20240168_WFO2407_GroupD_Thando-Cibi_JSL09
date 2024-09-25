34870d1d1d688b6722ac09ab02fef800cf16a9e4
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-25 15:17:51 +0200

    display author's name

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 // URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
 // (Change the "query" at the end to preferred theme)
 // Change the body's backgroundImage to: `url(<insert the URL of the image from the API here>)`
+const author = document.getElementById("author")
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=textures-patterns")
     .then(response => response.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.full})`
+        //display author's name
+        author.textContent = `By: ${data.user.name}`
     })
 
