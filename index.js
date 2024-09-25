aa7a81687ff59edac84a91be3c48630a7b8fa146
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-25 16:57:50 +0200

    Get crypto data

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const author = document.getElementById("author")
+
 //Get a random image from Unsplash and set it as the background
 // URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
 // (Change the "query" at the end to preferred theme)
 // Change the body's backgroundImage to: `url(<insert the URL of the image from the API here>)`
-const author = document.getElementById("author")
-
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=textures-patterns")
     .then(response => response.json())
     .then(data => {
@@ -15,4 +15,11 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1613679169453-ef00f3730ed4?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjcyNzM2NjF8&ixlib=rb-4.0.3&q=85)`
         author.textContent = `Photo by: Wolfgang Hasselmann`
     })
+
+// Pull down cryptocurrency data for dogecoin from the CoinGecko API
+// https://api.coingecko.com/api/v3/coins/dogecoin
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
 
